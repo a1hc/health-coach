8b7edf401ca325743ceb27c9d56c3df3a40f56ff
emily <jej001@ucsd.edu>
2018-01-30 22:56:04 -0800

    Implemented modal messages and added embedded Youtube videos

--- js/question.js
+++ js/question.js
@@ -6,39 +6,85 @@ $(document).ready(function(){
 	for(i = 1; i <= QUESTIONS.number; i++){
 		$("button#q-" + i).on("click", navigate);
 	}
+	$("form input").on("change", updateResponse);
+
+    $("#message").on('hide.bs.modal', function(){
+        var videoIFrame = $(this).find("iframe");
+        var videoSrc = videoIFrame.attr('src');
+        videoIFrame.attr('src', '');
+        videoIFrame.attr('src', videoSrc);
+    });
 });
+
+//Update User Response
+function updateResponse(){
+	Qnum = getQuestion();
+	if(Qnum < 0 || Qnum > 4) return false;
+	QType = QUESTIONS[Qnum];
+	const formName = "questionnaire";
+	const form = document.forms[formName];
+	const userInput = form.elements[QType].value;
+	
+	INPUT[QType] = userInput;
+	console.log(INPUT);
+}
 
 function modalMessage(){
 	$this = $(this);
 	const qType = ($this.find("input").attr("name"));
-	const response = $this.text();
+	const response = $this.find("input").val();
+	console.log(response);
 	switch(qType){
 		case QTYPE.BREAKFAST:
-			if(response === "Yes"){
+			if(response === "yes"){
+				$('#message').modal('toggle');
 				$(".modal-title").text("Breakfast Can Help");
-				$(".modal-body").text("Yes! Eating breakfast can boost your metabolism.");
+				$(".modal-body p").text("Yes! Eating breakfast can boost your metabolism.");
+				$("#video").hide();
 			}
 			else{
-				$(".modal-body").text("Place Holder");
+				if(INPUT.gender === "female"){
+					$('#message').modal('toggle');
+					$(".modal-title").text("Diet is important!");
+					$(".modal-body p").text("You have a fairly good diet. I think you might like this video: <Test Video 1>");
+					var videoIFrame = $('#message').find("iframe");
+					var videoSrc = videoIFrame.attr('src');
+					videoIFrame.attr('src', 'https://www.youtube.com/embed/C0DPdy98e4c');	
+					$("#video").show();
+				}
+				else{
+					$('#message').modal('toggle');
+					$(".modal-title").text("Diet is important!");
+					$(".modal-body p").text("You have a fairly good diet. I think you might like this video: <Test Video 2>");
+					var videoIFrame = $('#message').find("iframe");
+					var videoSrc = videoIFrame.attr('src');
+					videoIFrame.attr('src', 'https://www.youtube.com/embed/Es44QTJmuZ0');	
+					$("#video").show();
+				}
 			}
 			break;
 		case QTYPE.SLEEP:
-			if(response === "Yes"){
+			if(response === "yes"){
+				$('#message').modal('toggle');
 				$(".modal-title").text("Sleeping Enough Can Help");
-				$(".modal-body").text("Yes! You are getting the healthy amount of sleep.");
+				$(".modal-body p").text("Yes! You are getting the healthy amount of sleep.");
+				$("#video").hide();
 			}
 			else{
-				$(".modal-body").text("Place Holder");
+				if(INPUT.gender === "male"){
+					$('#message').modal('toggle');
+					$(".modal-title").text("Sleeping is important!");
+					$(".modal-body p").text("Sleep does wonders! I think you might like this <http://www.rxfitpro.com/product/antense/>.");
+					$("#video").hide();
+				}
 			}
 			break;
 		case QTYPE.EXERCISEHR:
-			if(response === "0-5 Hours"){
-				$(".modal-title").text("Exercising Can Help");
-				$(".modal-body").text("Yes! Exercising can improve your physical strength and fitness!");
-			}
-			else{
-				$(".modal-title").text("Exercising Can Help");
-				$(".modal-body").text("Yes! Exercising can improve your physical strength and fitness!");
+			if(response === "0-5" && INPUT.gender === "male" && INPUT.sleep === "no"){
+				$('#message').modal('toggle');
+				$(".modal-title").text("Let's talk!");
+				$(".modal-body p").text("You and I should talk. Let’s schedule a consultation.");
+				$("#video").hide();
 			}
 			break;
 		default:
@@ -131,14 +177,18 @@ function getQuestion(){
 }
 
 const INPUT = {
-	"gender" : "",
-	"eatBreakfast" : false,
-	"sleepGreater" : false,
-	"exerciseGreater": false
+	"gender" : null,
+	"breakfast" : null,
+	"sleep" : null,
+	"exercise": null
 };
 
 const QUESTIONS = {
-	"number" : 4
+	"number" : 4,
+	"1" : "gender",
+	"2" : "breakfast",
+	"3" : "sleep",
+	"4" : "exercise"
 }
 
 const QTYPE ={
